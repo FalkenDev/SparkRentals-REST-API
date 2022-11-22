3cd81b7fbdb10d001411a489901e8b8e928c9bfe
Kasper Falk <kafa21@student.bth.se>
2022-11-22 13:53:18 +0100

    Implemented admin login and create JWT token

--- v1/models/auth.js
+++ v1/models/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const hat = require("hat"); // for creating api key
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
-/*const { MongoClient, ObjectId } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const mongoURI = "mongodb://localhost:27017";
-let api_collection = db.collection("api");
+/*let api_collection = db.collection("api");
 let user_collection = db.collection("users");
 let admin_collection = db.collection("admin");*/
 
@@ -15,6 +17,8 @@ try {
 }
 
 const api_token = process.env.API_TOKEN
+
+const jwtSecret = process.env.JWT_SECRET;
 
 
 const auth = {
@@ -35,8 +39,73 @@ const auth = {
         }
     },
 
-    adminLogin: function() { // Admin login
+    adminLogin: async function(res, body) { // Admin login
+        const adminEmail = body.email;
+        const adminPassword = body.password;
+        const apiKey = body.apiKey;
 
+        if (!adminEmail || !adminPassword) {
+            return res.status(401).json({
+                errors: {
+                    status: 401,
+                    source: "/login",
+                    title: "Email or password missing",
+                    detail: "Email or password missing in request"
+                }
+            });
+        }
+
+        let client = new MongoClient(mongoURI);
+        let db = client.db("spark-rentals");
+        let admins_collection = db.collection("admins");
+        let admin = await admins_collection.findOne({email: adminEmail});
+
+        if (admin === null) {
+            return res.status(401).json({
+                errors: {
+                    status: 401,
+                    source: "/admin/login",
+                    title: "User not found",
+                    detail: "User with provided email not found."
+                }
+            });
+        }
+
+        bcrypt.compare(adminPassword, admin.password, (err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    errors: {
+                        status: 500,
+                        source: "/admin/login",
+                        title: "bcrypt error",
+                        detail: "bcrypt error"
+                    }
+                });
+            }
+
+            if (result) {
+                let payload = { api_key: apiKey, email: admin.email };
+                let jwtToken = jwt.sign(payload, jwtSecret, { expiresIn: '24h' });
+
+                return res.json({
+                    data: {
+                        type: "success",
+                        message: "User logged in",
+                        user: payload,
+                        token: jwtToken
+                    }
+                });
+            }
+
+            return res.status(401).json({
+                errors: {
+                    status: 401,
+                    source: "/login",
+                    title: "Wrong password",
+                    detail: "Password is incorrect."
+                }
+            });
+        });
     },
 
     adminRegister: function() { // Register a new Admin
