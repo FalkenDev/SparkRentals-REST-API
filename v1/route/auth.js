e2d13767d42d31fca54058a9f774b0c4c5b65c18
Kasper Falk <kafa21@student.bth.se>
2022-11-26 00:48:58 +0100

    Moved admin login route to admins folder instead

--- v1/route/auth.js
+++ v1/route/auth.js
@@ -2,11 +2,5 @@ const express = require('express');
 const router = express.Router();
 const authModel = require("../models/auth.js");
 
-router.post('/admin/login',
-    (req, res) => authModel.adminLogin(res, req.body));
-
-router.post('/admin',
-    (req, res, next) => authModel.adminCheckToken(req, res, next),
-    (req, res) => authModel.adminRegister(res, req.body));
 
 module.exports = router;
