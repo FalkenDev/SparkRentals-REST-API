e33a5539b55217f03824756d42ded4d7647c879f
Kasper Falk <kafa21@student.bth.se>
2022-11-26 00:33:26 +0100

    Added eslint

--- v1/index.js
+++ v1/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require("path");
 const router = express.Router();
 const authModel = require("./models/auth.js");
-const auth = require("./route/auth.js");
+const routeAuth = require("./route/auth.js");
+const routeAdmins = require("./route/admins.js");
 
 // MongoDb Database
 const { MongoClient, ObjectId } = require("mongodb");
@@ -10,9 +11,13 @@ const mongoURI = "mongodb://localhost:27017";
 
 //router.all('*', authModel.checkAPIKey);
 
-router.get('/', (req, res) => res.sendFile(path.join(__dirname + '/documentation.html')));
+router.get('/',
+    (req, res, next) => authModel.adminCheckToken(req, res, next),
+    (req, res) => res.sendFile(path.join(__dirname + '/documentation.html')));
 
-router.use("/auth", auth);
+router.use("/auth", routeAuth);
+
+router.use("/admins", routeAdmins);
 
 router.use(function (req, res) {
     return res.status(404).json({
